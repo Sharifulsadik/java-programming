d367432285c98f5f961160138a2af5b7eea089e9
Shariful sadik <81006301+Sharifulsadik@users.noreply.github.com>
2021-03-23 18:04:15 +0600

    Add files via upload

---
+++ getter_and_setter.java
@@ -0,0 +1,37 @@
+
+import java.util.Scanner;
+
+
+public class getter_and_setter {
+    
+ String name;
+ int User_pin = 1234;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getUser_pin() {
+        return User_pin;
+    }
+
+    public void setUser_pin(int User_pin) {
+        this.User_pin = User_pin;
+    }
+    
+
+
+
+
+   
+
+        
+    
+    
+    
+    
+}
